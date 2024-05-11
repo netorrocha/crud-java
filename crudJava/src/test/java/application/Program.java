37ec5e9e860a5c5d4dff011e966ce7b1ceca6060
Raimundo Neto <raimundo18746@gmail.com>
2024-05-11 17:43:29 -0300

    Alterando a classe Program para o sistema executar

--- crudJava/src/test/java/application/Program.java
+++ crudJava/src/test/java/application/Program.java
@@ -1,11 +1,22 @@
 package application;
 
+import entities.Client;
+import entitiesDao.ClientDAO;
 import factory.ConnectionFactory;
+
+import java.sql.SQLException;
 
 
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         ConnectionFactory.getConnection();
+        Client client1 = new Client(null,"Annelise","anninhadonetao@gmail.com");
+        ClientDAO clientDAO = new ClientDAO();
+        try {
+            clientDAO.save(client1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
